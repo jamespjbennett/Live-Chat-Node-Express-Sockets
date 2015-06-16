49942d54c7fb955731651772dcbbe44c090cf7cb
jamespjbennett <jamespjbennett@hotmail.com>
2015-06-16 20:08:59 +0100

    initial site setup. Reading homepage route

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express') // use the express library
+
+var app = express(); //this creates our app
+
+var http = require('http');
+var server = http.createServer(app); //create an HTTP server for the express app
+var port = process.env.PORT || 3000;
+
+server.listen(port, function(){
+  console.log('server running')
+});
+
+var router = express.Router()
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+app.use('/', router);
+
+router.get('/', function(req, res){
+  res.render('index', {header: 'index'})
+})
